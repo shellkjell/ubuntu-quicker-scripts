b039f5bbabcaa57960479559a115f17b74bfd20b
shellkjell <simon@haak-it.se>
2018-12-30 15:49:19 +0100

    Added option to install development software in install script (-d option)

--- install.sh
+++ install.sh
@@ -5,6 +5,53 @@ CurrentDirName=`dirname "$(readlink -f "$0")"`
 source "$CurrentDirName/scripts/common.sh"
 source "$CurrentDirName/scripts/alwaysSudo.sh"
 
+installPackages="ranger"
+
+args=$(getopt -l "dev" -o "d" -- "$@")
+
+eval set -- "$args"
+
+while [ $# -ge 1 ]; do
+  case "$1" in
+    --)
+      shift
+      break
+      ;;
+    -d|--dev)
+      printOut "common development environment software is going to be installed (option $1)"
+      installPackages="$installPackages build-essential git make curl \
+	      net-tools basez golang-go npm"
+
+      if [ command -v code ]
+      then
+      	printOut "vscode already installed, skipping"
+      else
+	printOut "vscode is going to be installed (option $1)"
+	# Add microsoft gpg key for vscode
+	curl -s https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg
+	# Create entry in sources.list.d
+	sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+        
+	# Restore permissions
+	chmod 770 /home/$SUDO_USER/.gnupg
+	chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.gnupg
+	
+	installPackages="$installPackages code"
+      fi
+      shift
+      ;;
+  esac
+
+  shift
+done
+
+installPackages=$((echo "$installPackages") | sed 's/[\n\t]/ /g' | xargs echo -n)
+
+printOut "Trying to install these packages:\n\033[1;32m$(echo "$installPackages" | xargs -n5 | sed -e 's/^/   /')\033[0m"
+
+apt update
+apt install $installPackages
+
 printOut "Linking scripts to /usr/local/bin"
 
 while read installScript 
